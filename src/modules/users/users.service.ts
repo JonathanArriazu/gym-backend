1b53503d4a470532beb6d6900a8d292e18f7c208
Jonathan Arriazu <95253006+JonathanArriazu@users.noreply.github.com>
2025-06-16 19:32:07 -0300

    Feat/user service filters (#3)
    * feat: isDeleted property added / email validations / search by user or name / isDeleted validation filter added
    
    * feat: pagination added to findAll user method
    
    * fix: logic removed because it is already used in the PaginationArgs DTO

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,29 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/common/prisma/prisma.service';
+import { PaginationArgs } from 'src/common/dto/pagination.dto';
+import { paginate } from 'src/common/pagination/pagination.utils';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  create(data: CreateUserDto) {
+  async create(data: CreateUserDto) {
+    const existingUser = await this.prisma.user.findFirst({
+    where: {
+      email: data.email,
+      isDeleted: false,
+    },
+  });
+
+   if (existingUser) {
+    throw new ConflictException('Ya existe un usuario con ese email');
+  }
+
     return this.prisma.user.create({ data });
   }
 
-  findAll() {
-    return this.prisma.user.findMany();
-  }
+  async findAll({ search, page = 1, limit = 10 }: PaginationArgs) {
+  
+  const where: Prisma.UserWhereInput = {
+    isDeleted: false,
+    OR: search
+      ? [
+          { email: { contains: search, mode: Prisma.QueryMode.insensitive } },
+          { name: { contains: search, mode: Prisma.QueryMode.insensitive } },
+        ]
+      : undefined,
+  };
 
-  findOne(id: number) {
-    return this.prisma.user.findUnique({ where: { id } });
+  return paginate(this.prisma.user, page, limit, where, { createdAt: 'desc' });
+}
+
+  async findOne(id: number) {
+    const user = await this.prisma.user.findUnique({ where: { id } });
+    return user?.isDeleted ? null : user;
   }
 
   update(id: number, data: UpdateUserDto) {
     return this.prisma.user.update({ where: { id }, data });
   }
 
-  remove(id: number) {
-    return this.prisma.user.delete({ where: { id } });
-  }
+  async remove(id: number) {
+  return this.prisma.user.update({
+    where: { id },
+    data: { isDeleted: true },
+  });
+}
 }
